bd1fbd4fffe2fd36ca41e0476d64f6d35bbf7bea
Zaide1 <69059158+Zaide1@users.noreply.github.com>
2024-01-30 15:23:21 +0000

    comments added to home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 
+/* The `<head>` section in HTML is used to define metadata about the HTML document. In this specific
+code snippet, the `<head>` section includes the following elements: */
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,11 +12,14 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </head>
 
+/* The code snippet is defining the navigation bar (`<nav>`) for the webpage. */
+
 <body>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
             <div class="collapse navbar-collapse" id="navbarText">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                /* The code snippet you provided is creating a navigation bar for a webpage. */
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="./">Home</a>
                     </li>
@@ -26,6 +32,7 @@
                 </ul>
             </div>
         </div>
+   /* The code snippet you provided is defining the content of the webpage after the navigation bar. */
     </nav>
     <h1>Final Year Module Selector</h1>
     <p><a href="./studentlisting">Student List</a></p>
